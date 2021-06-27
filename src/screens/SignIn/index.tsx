2b6e765ce404c1b42f541108f3dc426938692a4b
Maicon Lourenço <maiconloure@gmail.com>
2021-06-26 21:49:45 -0300

    added ButtonIcon and theme

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,41 @@
 import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, Image, StatusBar } from "react-native";
+import { ButtonIcon } from '../../components/ButtonIcon';
 import { styles } from './styles';
 
+import illustrationImg from '../../assets/illustration.png';
+
 export function SignIn() {
-  const [text, setText] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Sign In</Text>
-      <TextInput style={styles.input} onChangeText={setText} />
-      <Text>Você digitou: {text}</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image
+        source={illustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize{`\n`}
+          suas jogatinas{`\n`}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon 
+          title="Entrar com Discord" 
+          activeOpacity={0.7} 
+        />
+      </View>
     </View>
   );
 }
