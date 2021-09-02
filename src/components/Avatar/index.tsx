85d4c93e5f55a35557e016efe22c9caf89944295
Maicon Lourenço <maiconloure@gmail.com>
2021-09-01 23:21:01 -0300

    implements Avatar and add react-navigation

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import { theme } from '../../global/styles/theme';
+import { LinearGradient } from 'expo-linear-gradient';
+import { styles } from './styles';
+
+type Props = {
+  urlImage: string;
+}
+export function Avatar({ urlImage }: Props) {
+  const { secondary40, secondary70 } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[secondary40, secondary70]}
+    >
+      <Image
+        source={{ uri: urlImage }}
+        style={styles.avatar}
+      />
+    </LinearGradient>
+  )
+}
